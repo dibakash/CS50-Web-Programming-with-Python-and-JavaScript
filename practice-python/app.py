fe8f62e50cc23dd44125897db3810dc8eec73366
dibakash <dibakash@users.noreply.github.com>
2021-06-26 08:51:42 +0530

    26 Jun

--- practice-python/app.py
+++ practice-python/app.py
@@ -1,1 +1,24 @@
 # new file ready to roll
+class Flight():
+    def __init__(self, available_seats):
+        self.available_seats = available_seats
+        self.passengers = {}
+
+    def bookSeat(self, person):
+        remaining_seats = self.available_seats
+        if not remaining_seats:
+            print(f'seats not available for {person}')
+            return
+        else:
+            self.passengers[person] = "booked"
+            self.available_seats -= 1
+
+
+people = ["tony", "steve", "peter", "nina", "howrang"]
+
+mig32 = Flight(3)
+
+for guy in people:
+    mig32.bookSeat(guy)
+
+print(f"passenger: {mig32.passengers}")
