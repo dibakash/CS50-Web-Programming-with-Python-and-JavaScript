0e155a361079d3f14116aab73bd047329f70d5ff
dibakash <dibakash@users.noreply.github.com>
2021-07-17 11:03:36 +0530

    17th Jul

--- practice-python/app.py
+++ practice-python/app.py
@@ -1,16 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
+REGISTRANTS = {}
+
+SPORTS = ["cricket", "MMA", "soccer", "skating", "football"]
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORTS)
 
 
-@app.route("/greet")
-def greet():
-    name = request.args.get("name", "there")
-    if name == "":
-        return render_template("greet.html", name="There")
-    return render_template("greet.html", name=name)
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+    if not name:
+        return render_template("failure.html", message="Name not provided")
+    if not sport:
+        return render_template("failure.html", message="Sport not provided")
+    if sport not in SPORTS:
+        return render_template("failure.html", message="Incorrect Sports")
+
+    REGISTRANTS[name] = sport
+
+    return render_template("success.html", firstName=name, sport=sport)
+
+
+@app.route("/registrants")
+def registrants():
+    return render_template("registrants.html", registrants=REGISTRANTS)
